8f6dbdc7c83051d22dd278bd71735f63681e4fa5
Adricego <05.diegocortes@gmail.com>
2025-10-19 16:00:26 -0300

    first commit

---
+++ RETO 00/Reto00.py
@@ -0,0 +1,28 @@
+#utilizaré Python para desarrollar estos retos https://www.python.org
+
+#Este es un comentario
+print("Hola, Mundo") #También se pueden ingresar al final de una línea de código.
+
+#Se pueden hacer
+#comentarios
+#en varias líneas
+
+"""
+Esto es un comentarios 
+escríto en varias líneas
+igual que el anterior, para esto
+se usan tres comillas dobles ("").
+"""
+
+name  = "Juan" #Asignación de variable con nombre y valor
+age   = 25    #Sin asignarle nombre a la variable (o sea, sin nombre)
+
+age = 25
+pi =  3.14159
+name = "Diego"
+hola = True
+chao = False
+
+lenguaje = "Python"
+
+print (f"hola, {lenguaje}")
